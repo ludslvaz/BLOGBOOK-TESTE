5cf6e4919a3fc2f35294d2490cbe3023aed1f7b1
Veetorio <ettorevitorio98@gmail.com>
2024-02-02 07:44:00 -0300

    poggers 2

--- BlogBook - teste/js/scriptLeftNav.js
+++ BlogBook - teste/js/scriptLeftNav.js
@@ -1,4 +1,4 @@
 let analises = document.getElementById('analises')
 analises.addEventListener("click", function() {
-    window.location.href = ""
+    window.location.href = "https://ludslvaz.github.io/BLOGBOOK-TESTE/BlogBook%20-%20teste/analises%20page/index.html#"
 })
