ad6656a7f56522b8dd16eaa798cafeba642110f0
Ludmilla Vaz <ludmillaslvaz@gmail.com>
2024-02-02 08:44:15 -0300

    .

---
+++ BlogBook - teste/js/sriptUser.js
@@ -0,0 +1,8 @@
+let btnAnalises = document.getElementById('btnAnalises')
+btnAnalises.addEventListener("click", function() {
+    window.location.href = "../profile page/index2.html"
+})
+let btnNoticias = document.getElementById('btnNoticias')
+btnNoticias.addEventListener("click", function() {
+    window.location.href = "../profile page/index.html"
+})
