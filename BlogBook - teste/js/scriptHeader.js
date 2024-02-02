66f87fa71891ad8d1ec43a76003c6b8ebcd676e9
Ludmilla Vaz <ludmillaslvaz@gmail.com>
2024-02-02 07:58:28 -0300

    .

--- BlogBook - teste/js/scriptHeader.js
+++ BlogBook - teste/js/scriptHeader.js
@@ -1,6 +1,6 @@
 let entrarBtn = document.getElementById('entrarBtn')
 entrarBtn.addEventListener("click", function() {
-    window.location.href = "http://127.0.0.1:5500/BlogBook%20-%20teste/tela%20de%20login/index.html"
+    window.location.href = "../tela de login/index.html"
 })
 
 let createContBtn = document.getElementById('createContBtn')
