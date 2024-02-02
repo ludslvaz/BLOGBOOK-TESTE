2aec2addd963bc9a64ee5202c30bf589b994317b
Ludmilla Vaz <ludmillaslvaz@gmail.com>
2024-02-02 08:05:48 -0300

    .

--- BlogBook - teste/js/scriptLeftNav.js
+++ BlogBook - teste/js/scriptLeftNav.js
@@ -2,3 +2,7 @@ let analises = document.getElementById('analises')
 analises.addEventListener("click", function() {
     window.location.href = "../analises page/index.html"
 })
+let pgPrincipal = document.getElementById('pgPrincipal')
+pgPrincipal.addEventListener("click", function() {
+    window.location.href = "../blog/index.html"
+})
